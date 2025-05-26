9f91ac35e3864ec89e6baa859b3c5de769cf3d92
rasnad <lucaskpo987@gmail.com>
2025-05-25 21:39:25 -0300

    Adding entityts

---
+++ JavaApplication26/src/obligatorio/Gestor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package obligatorio;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author lucas
+ */
+public class Gestor {
+ UnidadProcesadora ubnidadProcesadora;
+ ArrayList<Pedido> pedidos;
+}
