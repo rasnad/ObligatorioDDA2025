b17695ba80052728694bc07561e3c375528cfaf5
Lucas Espinosa <85807454+rasnad@users.noreply.github.com>
2025-05-25 21:52:57 -0300

    Adding entityts (#1)
    * Adding entityts
    
    * new name

---
+++ ObligatorioProyecto/src/obligatorio/Insumo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package obligatorio;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author lucas
+ */
+public class Insumo {
+    ArrayList<Ingrediente> igredientes;
+}
---
+++ ObligatorioProyecto/src/obligatorio/UnidadProcesadora.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package obligatorio;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author lucas
+ */
+public class UnidadProcesadora {
+   ArrayList<Gestor> gestores; 
+   ArrayList<Pedido> pedidos;
+   Item item;
+}
