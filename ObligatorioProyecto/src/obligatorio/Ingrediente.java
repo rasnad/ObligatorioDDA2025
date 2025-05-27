69bebd4c77dcda7d645749ef5809d59cc51fbc3b
NicolasDelCore <70823142+NicolasDelCore@users.noreply.github.com>
2025-05-26 22:13:34 -0300

    Ingrediente v0
    -Ingrediente no tiene un item.
    -Ingrediente tiene una cantidad int para el insumo.
    -Removí comentarios automáticos de NetBeans.

--- ObligatorioProyecto/src/obligatorio/Ingrediente.java
+++ ObligatorioProyecto/src/obligatorio/Ingrediente.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package obligatorio;
 
-/**
- *
- * @author lucas
- */
 public class Ingrediente {
-    Item item;
+    int cantidad;
     Insumo insumo;
 }
