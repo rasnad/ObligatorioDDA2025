65ac91218bc53600b983770e61c1de6339bf6529
NicolasDelCore <70823142+NicolasDelCore@users.noreply.github.com>
2025-05-26 22:11:03 -0300

    Item v0
    -Item no necesita tener un Pedido.
    -Removí comentarios automáticos de NetBeans.

--- ObligatorioProyecto/src/obligatorio/Item.java
+++ ObligatorioProyecto/src/obligatorio/Item.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package obligatorio;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author lucas
- */
 public class Item {
     ArrayList<Ingrediente> igredientes;
     UnidadProcesadora unidadProcesadora;
     Categoria categoria;
-    Pedido pedido; // El pedido, puede, o no estar atado a un Producto ? Es decir 0 a 1
 }
