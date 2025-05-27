a01dfbe9fb7f35b2214d724feb8895840d769e65
NicolasDelCore <70823142+NicolasDelCore@users.noreply.github.com>
2025-05-26 22:33:00 -0300

    UnidadProcesadora v0
    -UnidadProcesadora no tiene un Item, tiene una lista de Items (esto no se veía bien en el diagrama, así que corregí el diagrama también).
    -Removí comentarios automáticos de NetBeans.

--- ObligatorioProyecto/src/obligatorio/UnidadProcesadora.java
+++ ObligatorioProyecto/src/obligatorio/UnidadProcesadora.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package obligatorio;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author lucas
- */
 public class UnidadProcesadora {
-   ArrayList<Gestor> gestores; 
+   ArrayList<Gestor> gestores;
    ArrayList<Pedido> pedidos;
-   Item item;
+   ArrayList<Item> items;
 }
