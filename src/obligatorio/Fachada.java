663f97a4658fa44814b9e18f92b767b4a71f54f4
Nicolas Del Core <70823142+NicolasDelCore@users.noreply.github.com>
2025-06-09 19:42:03 -0300

    Implementación de Singleton en clase Fachada.

--- src/obligatorio/Fachada.java
+++ src/obligatorio/Fachada.java
@@ -1,9 +1,18 @@
 package obligatorio;
 
 public class Fachada {
-
+    
+    private static Fachada instancia = new Fachada();
     private SubsistemaAcceso subsistemaAcceso;
     private SubsistemaServicio subsistemaServicio;
+    
+    //Constructor Singleton
+    private Fachada(){}
+    
+    //Retorno de Instancia
+    public static Fachada getInstancia(){
+        return instancia;
+    }
 
     public void loginCliente(Dispositivo dispositivo, String username, String password){
         subsistemaAcceso.loginCliente(dispositivo, username, password );
